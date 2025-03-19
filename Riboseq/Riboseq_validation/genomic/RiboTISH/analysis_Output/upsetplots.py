0d19033f5dc983f9541d7f24c191bf723104ee33
Christina Kalk <ckalk@bohr.bio-inf.de>
2025-03-19 17:50:05 +0100

    upsetplots RiboTISH with custom order

--- Riboseq/Riboseq_validation/genomic/RiboTISH/analysis_Output/upsetplots.py
+++ Riboseq/Riboseq_validation/genomic/RiboTISH/analysis_Output/upsetplots.py
@@ -8,15 +8,19 @@ import pandas as pd
 import upsetplot
 from upsetplot import from_contents
 from pybedtools import BedTool
+import matplotlib.pyplot as plt
 
 
 TransAI_scripts_path = "/home/ckalk/scripts/SplitORFs/LLMs/TranslationAI/analyze_TIS/"
 sys.path.append(os.path.dirname(TransAI_scripts_path))
 
 
-Ribo_coverage_path = '/projects/splitorfs/work/Riboseq/Output/Riboseq_genomic_single_samples/resample/NMD_genome'
-RiboTISH_path = '/projects/splitorfs/work/Riboseq/Output/RiboTISH_NMD_custom'
-datatype = 'NMD'
+RiboTISH_path = sys.argv[1]
+datatype = sys.argv[2]
+
+
+# RiboTISH_path = '/projects/splitorfs/work/Riboseq/Output/RiboTISH_NMD_custom'
+# datatype = 'NMD'
 
 
 # here comes the import
@@ -44,13 +48,21 @@ def main():
             )
 
     Upsetplot_data = from_contents(RiboTISH_hits_dict)
+
     custom_order = ["OHMX20220060_001", "OHMX20220060_002", "OHMX20220060_003", "OHMX20220060_004",
                     "OHMX20220060_005", "OHMX20220060_006", "ERR3367797", "ERR3367798"]  # Define your preferred order
 
     # Convert index to categorical with custom order
     Upsetplot_data = Upsetplot_data.reorder_levels(custom_order)
 
-    upsetplot.plot(Upsetplot_data, sort_categories_by='-input')
+    fig = plt.figure(figsize=(8, 6))  # Create a figure
+
+    upsetplot.plot(Upsetplot_data, sort_categories_by='-input',
+                   sort_by='-degree')
+
+    # Save the plot as an SVG file
+    plt.savefig(f"{RiboTISH_path}/plots/{datatype}_upset_plot.svg",
+                format="svg", bbox_inches="tight")
 
 
 if __name__ == "__main__":
