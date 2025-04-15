4fbeb4742b1a6a6d21b3893bc0b666e601d1c788
Christina Kalk <ckalk@bohr.bio-inf.de>
2025-04-15 11:03:49 +0200

    contaminant alignment

--- Riboseq/Michi_Vlado_round_1/extract_compare_umis.py
+++ Riboseq/Michi_Vlado_round_1/extract_compare_umis.py
@@ -13,11 +13,12 @@ files = os.listdir(fastq_path)
 # Filter out only files (if needed)
 files = [f for f in files if os.path.isfile(os.path.join(
     fastq_path, f)) and 'huvec' in f and 'R1' in f and f.endswith('fastq.gz')]
-print(files)
+print(files, flush=True)
 
 
 for file in files:
     file = os.path.join(fastq_path, file)
+    print(file, flush=True)
     read2_file = file.replace('R1', 'R2')
     # Open the FASTQ files
     with gzip.open(file, "rt") as fwd_file, \
@@ -30,12 +31,21 @@ for file in files:
         nr_umi_match = 0
         umi_trimmed_fwd_reads = []
         umi_trimmed_rev_reads = []
+        nr_wrong_umi_rev = 0
+        nr_wrong_umi_forward = 0
         # Iterate through the paired reads
         for fwd, rev in zip(fwd_reads, rev_reads):
             nr_paired_reads = nr_paired_reads + 1
             # Extract UMI sequences (first 9 bases for forward, last 9 for reverse)
             fwd_umi = fwd.seq[:9]
             rev_umi = rev.seq[-9:]
+
+            # assert fwd_umi[-1] != 'A'
+            # assert rev_umi[0] != 'T'
+            if fwd_umi[-1] == 'A':
+                nr_wrong_umi_forward += 1
+            if rev_umi[0] == 'T':
+                nr_wrong_umi_rev += 1
 
             # create new header name with both UMis attached
             fwd_id = fwd.id + '_' + fwd_umi + '_' + rev_umi
@@ -59,9 +69,14 @@ for file in files:
                 nr_umi_match += 1
             else:
                 nr_no_umi_match += 1
-        print('nr paired reads', nr_paired_reads)
-        print('nr times UMI matches', nr_umi_match)
-        print('nr of times UMI does not match', nr_no_umi_match)
+        print('nr paired reads', nr_paired_reads, flush=True)
+        print('nr times UMI matches', nr_umi_match, flush=True)
+        print('nr of times UMI does not match', nr_no_umi_match, flush=True)
+
+        print('Nr of UMI with A in 9th position, not supposed to:',
+              nr_wrong_umi_forward)
+        print('Nr of rev UMI with T in first position, not supposed to:',
+              nr_wrong_umi_rev)
 
     forward_trimmed_file_name = umi_trimmed_path + '/' + \
         os.path.basename(file).split(
