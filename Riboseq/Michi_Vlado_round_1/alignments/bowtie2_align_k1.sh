6d7caf711cb3bdf558e0b65b6b4a0f5f15108489
Christina Kalk <ckalk@curie.bio-inf.de>
2025-04-29 10:51:49 +0200

    summarize mappings bowtie2 transcriptome raw and mapping percentages

---
+++ Riboseq/Michi_Vlado_round_1/alignments/bowtie2_align_k1.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+Bowtie2_base_name=$1
+Bowtie_ref_fasta=$2
+FASTQ_Inpath=$3
+Bowtie_out_dir=$4
+aligned_name=$5
+
+if [[ ${Bowtie_ref_fasta} != "no_index" ]]; then
+    bowtie2-build ${Bowtie_ref_fasta} ${Bowtie2_base_name} --threads 32
+fi
+
+# First try matching *R1* files
+shopt -s nullglob  # So non-matching globs result in empty arrays
+files=("${FASTQ_Inpath}"/*R1*.fastq.gz)
+
+if [ ${#files[@]} -eq 0 ]; then
+    files=("${FASTQ_Inpath}"/*.fastq.1.gz)
+fi
+
+
+for FQ in "${files[@]}"
+do
+    sample=$(basename "$FQ")       # remove path
+    if [[ ${sample} == *"R1"* ]]; then
+        sample=${sample%%R1*}          # remove R1 and everything after
+        FQ2=${FQ/R1/R2} # substitute R1 with R2 in the whole file path
+    else
+        sample=${sample%%.fastq*} 
+        FQ2=${FQ/.1/.2} # substitute R1 with R2 in the whole file path
+    fi
+    # echo ${sample}
+    # echo ${FQ}
+    # echo ${FQ2}
+    # bowtie2 \
+    # -p 32 \
+    # --np 3 \
+    # --very-sensitive-local \
+    # --no-discordant \
+    # --ignore-quals \
+    # --score-min L,0,1.8 \
+    # -S "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1.sam \
+    # --un-conc-gz "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_unaligned.fastq.gz \
+    # -x ${Bowtie2_base_name} \
+    # -q \
+    # -1 ${FQ} \
+    # -2 ${FQ2}
+
+    # samtools view -@ 32 -bS "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1.sam \
+    #  > "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1.bam
+
+    samtools sort -@ 32 -o "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1_sorted.bam \
+    "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1.bam
+
+    # remove secondary and supplementary alignments
+    # -f 0x2: as well as only keep properly paired reads
+    # -f 0x40 keep only the first read
+    samtools view -F 256 -F 2048 -f 0x2 -f 0x40 -b "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1_sorted.bam > \
+     "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1_sorted_filtered.bam
+   
+
+    samtools index --threads 32 "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1_sorted_filtered.bam
+
+    samtools idxstats "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1_sorted_filtered.bam > \
+    "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_idxstats.out
+
+
+
+    rm "${Bowtie_out_dir}"/"${sample}"bowtie2_${aligned_name}_k1.sam
+done
+# report up to 100 alignments
+# high penalty for mismatching bases in the seed
+# -N 1 \ allow for one mismatch in the seed alignment, because of the polyA
+# -L 20 \   # 20 is actually the default in local mode
--- Riboseq/Michi_Vlado_round_1/alignments/summarize_bowtie2_alns_by_source.py
+++ Riboseq/Michi_Vlado_round_1/alignments/summarize_bowtie2_alns_by_source.py
@@ -4,7 +4,6 @@ import os.path
 from Bio import SeqIO
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 
 def assign_trans_IDs_to_source(ref_fastas):
@@ -25,7 +24,7 @@ def assign_trans_IDs_to_source(ref_fastas):
     return reference_dict
 
 
-def plot_summary(summary_mapping_stats_df, outdir):
+def plot_summary(summary_mapping_stats_df, outdir, raw=False):
     """
         plot summary statistics of the mapping to Ignolia like reference
 
@@ -51,13 +50,14 @@ def plot_summary(summary_mapping_stats_df, outdir):
                                                                             'MANE.GRCh38.v0.95.select_ensembl_rna': 'MANE_mRNA',
                                                                             'hg38-tRNAs': 'tRNA',
                                                                             'rRNA_ref_NCBI_Ens': 'rRNA'})
+    # Make one pd.Series per source
     mRNA_df = summary_mapping_stats_df_t["MANE_mRNA"]
     rRNA_df = summary_mapping_stats_df_t["rRNA"]
     ncRNA_df = summary_mapping_stats_df_t["ncRNA"]
     mt_df = summary_mapping_stats_df_t["MT"]
     tRNA_df = summary_mapping_stats_df_t["tRNA"]
 
-    x = ['_'.join(x[2:7]) for x in mRNA_df.index.str.split('_')]
+    x = ['_'.join(x[2:8]) for x in mRNA_df.index.str.split('_')]
 
     plt.bar(x, ncRNA_df,
             color='#40A3CD', label='ncRNA')
@@ -76,16 +76,29 @@ def plot_summary(summary_mapping_stats_df, outdir):
     add_labels(x, tRNA_df, ncRNA_df)
     add_labels(x, ncRNA_df, None)
 
+    if raw:
+        unmapped_filtered_df = summary_mapping_stats_df_t["filtered_unmapped"]
+        plt.bar(x, unmapped_filtered_df, bottom=ncRNA_df +
+                tRNA_df + rRNA_df + mt_df + mRNA_df,
+                color='#FDDA0D', label='filtered_unmapped')
+        add_labels(x, unmapped_filtered_df, ncRNA_df +
+                   tRNA_df + rRNA_df + mt_df + mRNA_df)
+
     plt.ylabel('Percentage')
-    plt.xticks(rotation=45)
-    plt.legend(title='RNA Type')
+    plt.xticks(rotation=90)
+    plt.legend(title='RNA Type', loc='upper left', bbox_to_anchor=(1, 1))
     plt.title('Stacked Mapping Percentages per Sample')
 
-    plt.savefig(os.path.join(outdir, "barplot_mapping_stats_per_sample.png"))
+    if raw:
+        plt.savefig(os.path.join(
+            outdir, "barplot_mapping_stats_per_sample_raw.png"), bbox_inches='tight')
+    else:
+        plt.savefig(os.path.join(
+            outdir, "barplot_mapping_stats_per_sample.png"), bbox_inches='tight')
     plt.close()
 
 
-def summarize_mapping_statistics(reference_dict, idx_stats):
+def summarize_mapping_statistics(reference_dict, idx_stats, raw=False):
     """
     Obtain mapping percentages of references from samtools idxstats.
 
@@ -93,6 +106,14 @@ def summarize_mapping_statistics(reference_dict, idx_stats):
     Summarize the alignments by source from mapping stats of single transcripts.
 
     """
+    raw_read_counts_dict = {'uf_muellermcnicoll_2025_04_05_huvec_dhypo_3': 29144203,
+                            'uf_muellermcnicoll_2025_04_01_huvec_dnor_2': 25130356,
+                            'uf_muellermcnicoll_2025_04_02_huvec_dnor_3': 23952402,
+                            'uf_muellermcnicoll_2025_04_03_huvec_dnor_4': 29872996,
+                            'uf_muellermcnicoll_2025_04_04_huvec_dhypo_2': 26795853,
+                            'uf_muellermcnicoll_2025_04_06_huvec_dhypo_4': 29422604
+                            }
+
     summary_mapping_stats_df = None
     for idx_stat in idx_stats:
         idx_stat_df = pd.read_csv(idx_stat,
@@ -101,12 +122,27 @@ def summarize_mapping_statistics(reference_dict, idx_stats):
                                   header=None,
                                   names=['tID', 'length', 'mapped', 'unmapped'])
         idx_name = os.path.basename(idx_stat).split('.')[0]
+        # skip the last row that is just a star and two dots
         idx_stat_df = idx_stat_df.iloc[:-1, :]
+        # map the tids to their source
         idx_stat_df['source'] = idx_stat_df['tID'].map(reference_dict)
+        if raw:
+            nr_unmapped = raw_read_counts_dict[idx_name] - \
+                sum(idx_stat_df['mapped'])
+            add_unmapped_row = {'tID': 'unmapped', 'length': 0,
+                                'mapped': nr_unmapped, 'unmapped': 0, 'source': 'filtered_unmapped'}
+            idx_stat_df = pd.concat(
+                [idx_stat_df, pd.DataFrame([add_unmapped_row])], ignore_index=True)
+
+        # get the number of mapping reads per tID
         idx_stat_df_by_source = idx_stat_df[[
             'source', 'mapped']].groupby('source').agg('sum')
+
+        # calculate the mapping percentage per source of all mapping reads
+        # if raw equals to true than all the unmapped and filtered reads are counted as well
         idx_stat_df_by_source[f'{idx_name}_percentage'] = idx_stat_df_by_source['mapped'] / \
             sum(idx_stat_df_by_source['mapped'])
+
         if summary_mapping_stats_df is not None:
             summary_mapping_stats_df[f'{idx_name}_percentage'] = idx_stat_df_by_source[f'{idx_name}_percentage']
             summary_mapping_stats_df[f'{idx_name}_mapped'] = idx_stat_df_by_source['mapped']
@@ -114,16 +150,29 @@ def summarize_mapping_statistics(reference_dict, idx_stats):
             summary_mapping_stats_df = idx_stat_df_by_source.rename(
                 columns={'mapped': f'{idx_name}_mapped'})
 
+        # if raw:
+        #     idx_stat_df_by_source[f'{idx_name}_percentage'] = idx_stat_df_by_source['mapped'] / \
+        #         raw_read_counts_dict[idx_name]
+        #     idx_stat_df_by_source[f'{idx_name}_not_mapped_or_filtered']\
+        #         = raw_read_counts_dict[idx_name] - idx_stat_df_by_source['mapped']
+        #     idx_stat_df_by_source[f'{idx_name}_not_mapped_or_filtered_percentage']\
+        #         = (raw_read_counts_dict[idx_name] - idx_stat_df_by_source['mapped'])/raw_read_counts_dict[idx_name]
+        # else:
+
         ############### PLOT VIOLIN PLOTS INDIVIDUALLY ##########################################################
         fig, ax = plt.subplots()
         ax.pie(idx_stat_df_by_source['mapped'],
                labels=idx_stat_df_by_source.index, autopct='%1.1f%%')
         plt.title(f'Mapping statistics of {idx_name}')
-        plt.savefig(
-            os.path.join(os.path.dirname(idx_stat), f'{idx_name}_mapping_percentages.png'))
+        if raw:
+            plt.savefig(
+                os.path.join(os.path.dirname(idx_stat), f'{idx_name}_mapping_percentages_raw.png'))
+        else:
+            plt.savefig(
+                os.path.join(os.path.dirname(idx_stat), f'{idx_name}_mapping_percentages.png'))
         plt.close()
 
-    plot_summary(summary_mapping_stats_df, os.path.dirname(idx_stat))
+    plot_summary(summary_mapping_stats_df, os.path.dirname(idx_stat), raw)
 
     return summary_mapping_stats_df
 
@@ -141,7 +190,7 @@ def main():
     reference_dict = assign_trans_IDs_to_source(ref_fastas)
 
     summarized_map_stats_df = summarize_mapping_statistics(
-        reference_dict, idx_stats)
+        reference_dict, idx_stats, raw=True)  #
 
     out_dir = os.path.dirname(idx_stats[0])
     summarized_map_stats_df.to_csv(f'{out_dir}/summarized_map_stats_df.csv')
