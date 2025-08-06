0391b5f4846661457fde98759667c3faff739200
Christina Kalk <ckalk@bohr.bio-inf.de>
2025-08-06 15:52:18 +0200

    sqanti3 rules filter

---
+++ PacBio_analysis/mandalorion/assess_mando_sqanti3/sqanti3/sqanti_rules/sqanti3_rules_huvec_06_08_25.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+eval "$(conda shell.bash hook)"
+source activate sqanti3
+
+sqanti_path=$1
+sqanti_qc_outname=$2
+sqanti_filter_outdir=$3
+filter_json=$4
+
+
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib/gcc/x86_64-conda-linux-gnu/15.1.0:$LD_LIBRARY_PATH"
+
+
+python ${sqanti_path}/sqanti3_filter.py rules \
+--sqanti_class ${sqanti_qc_outname}_classification.txt \
+--filter_gtf ${sqanti_qc_outname}_corrected.gtf  \
+--filter_faa ${sqanti_qc_outname}_corrected.faa \
+--filter_isoforms ${sqanti_qc_outname}_corrected.fasta \
+-d ${sqanti_filter_outdir} \
+--json ${filter_json}
+
+
+
