fe9235f9e40f43c2cb39faf39acea6f4b6843d3b
Christina Kalk <kalk@med.uni-frankfurt.de>
2026-02-10 15:59:48 +0100

    Upsetplot with glioblastoma U343 - and correct UR not overlapping, consistent with pie chart

--- Riboseq/Riboseq_validation/genomic/resample_random/plotting/Upsetplot_SO_upset_by_type_paper_overlap_0.5_02_2026.R
+++ Riboseq/Riboseq_validation/genomic/resample_random/plotting/Upsetplot_SO_upset_by_type_paper_overlap_0.5_02_2026.R
@@ -87,15 +87,15 @@ cell_type_to_sample_dict <- list(
                             "SRR10533458", 
                             "SRR10533459", 
                             "SRR105334560"),
-     "glioblastoma U343" = c("SRR105334561",
-                            "SRR105334562", 
-                            "SRR105334563", 
-                            "SRR105334564", 
-                            "SRR105334565", 
-                            "SRR105334566",
-                            "SRR105334567", 
-                            "SRR105334568", 
-                            "SRR105334569")
+     "glioblastoma U343" = c("SRR10533461",
+                            "SRR10533462", 
+                            "SRR10533463", 
+                            "SRR10533464", 
+                            "SRR10533465", 
+                            "SRR10533466",
+                            "SRR10533467", 
+                            "SRR10533468", 
+                            "SRR10533469")
 )
 
 
@@ -111,21 +111,22 @@ upset_list_gen_regions_per_cell_type <- function(cell_type_to_sample_dict, valid
     for (cell_type in names(cell_type_to_sample_dict)) {
         samples_of_cell_type <- cell_type_to_sample_dict[cell_type]
         samples_of_cell_type <- unlist(samples_of_cell_type)[unlist(samples_of_cell_type) %in% colnames(validated_so_df)]
-        keep_cols <- c(samples_of_cell_type, c("OrfID"))
-        print(keep_cols)
+        keep_cols <- c(samples_of_cell_type, c("ID"))
         df_sample <- validated_so_df[,keep_cols]
 
         if (is.data.frame(df_sample) && nrow(df_sample) > 0){
-            print(rowSums(df_sample == "True", na.rm = TRUE))
-            df_sample_validated <- df_sample[rowSums(df_sample == "True", na.rm = TRUE) > 0, "OrfID"]
-            upsetlist_genomic[[cell_type]] <- unique(df_sample_validated$OrfID) 
+            df_sample_validated <- df_sample[rowSums(df_sample == "True", na.rm = TRUE) > 0, "ID"]
+            if (length(df_sample_validated > 0)) {
+            upsetlist_genomic[[cell_type]] <- unique(df_sample_validated) 
+            }
         }
+    }
     return(upsetlist_genomic)
 }
 
 
 get_upset_list_from_csv <- function(path, coarse) {
-    file_path <-  file.path(path, "validated_so_df.csv")
+    file_path <-  file.path(path, "val_dna_overlapping_ur_df.csv")
     validated_so_df <- read.csv(file_path, sep = ",")
 
     if (coarse){
@@ -159,13 +160,13 @@ if (coarse) {
 
 svg(file.path(nmd_path, nmd_out), width = 14, height = 6)
     upset(fromList(upsetlist_genomic_nmd), order.by = "freq", nsets = nsets, point.size = 2.25, line.size = 1.5, set_size.show = TRUE,
-        set_size.scale_max = set_size.scale_max_nmd,
+        set_size.scale_max = set_size.scale_max_nmd, nintersects=16, 
         mainbar.y.label = "NMD Unique Region Intersections", sets.x.label = "Matching NMD unique regions per cell type",
         text.scale = 1.7)
 dev.off()
 svg(file.path(ri_path, ri_out), width = 14, height = 6)
     upset(fromList(upsetlist_genomic_ri), order.by = "freq", nsets = nsets, point.size = 2.25, line.size = 1.5, set_size.show = TRUE,
-        set_size.scale_max = set_size.scale_max_ri, sets = sorted_samples_ri, 
+        set_size.scale_max = set_size.scale_max_ri, sets = sorted_samples_ri, nintersects = 19,
         mainbar.y.label = "RI Unique Region Intersections", sets.x.label = "Matching RI unique regions per cell type",
         text.scale = 1.7)
 dev.off()
