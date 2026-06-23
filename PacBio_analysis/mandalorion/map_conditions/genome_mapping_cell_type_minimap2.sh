fc08918ebaeceb6907cf0200b12c1b80f8789a9f
Christina Kalk <kalk@med.uni-frankfurt.de>
2026-06-23 11:45:53 +0200

    minimap2 for Stringtie

--- PacBio_analysis/mandalorion/map_conditions/genome_mapping_cell_type_minimap2.sh
+++ PacBio_analysis/mandalorion/map_conditions/genome_mapping_cell_type_minimap2.sh
@@ -77,19 +77,18 @@ mkdir -p "$out_path/"${cell_type}"/minimap2_align/genome_index"
 # might not want to index, as the parameters need to be adjusted for the datatype
 # and I simply could not find the informaiton about how to do this 
 # for my parameters
-#if [ ! -e "$out_path/"${cell_type}"/minimap2_align/genome_index/genome_index.mmi" ]; then
-    if [[ ! -e "$out_path/"${cell_type}/minimap2_align/genome_index/"$name_gtf"_junctions.bed ]]; then
-        name_gtf=$(basename "$gtf" .gtf)
-        paftools.js gff2bed "$gtf" > "$out_path/"${cell_type}/minimap2_align/genome_index/"$name_gtf"_junctions.bed
-    fi
-    # minimap2 -d "${out_path}"/"${cell_type}"/minimap2_align/genome_index/genome_index.mmi "${genome_fasta_file}"
-# fi
+
+if [[ ! -e "$out_path/"${cell_type}/minimap2_align/genome_index/"$name_gtf"_junctions.bed ]]; then
+    name_gtf=$(basename "$gtf" .gtf)
+    paftools.js gff2bed "$gtf" > "$out_path/"${cell_type}/minimap2_align/genome_index/"$name_gtf"_junctions.bed
+fi
+
 
 
 for fastq in "${isoseq_reads_dir}"/"${cell_type}"*fastq; do
     sample=$(basename "$fastq")
     sample="${sample%_merged_lima_refined.fastq}"
-    if [ ! -e "${out_path}"/"${cell_type}"/minimap2_align/"${sample}"_minimap2_aligned_genome.bam ]; then
+    if [ ! -e "${out_path}"/"${cell_type}"/minimap2_align/"${sample}"_minimap2_aligned_genome_sorted.bam ]; then
         echo "${out_path}"/"${cell_type}"/minimap2_align/"${sample}"_minimap2_aligned_genome.bam
         minimap2 -ax splice:hq -uf --junc-bed="$out_path/${cell_type}/minimap2_align/genome_index/"$name_gtf"_junctions.bed" -t 40\
         "$genome_fasta_file" \
@@ -98,21 +97,24 @@ for fastq in "${isoseq_reads_dir}"/"${cell_type}"*fastq; do
     fi
  done
 
+shopt -s nullglob
+bam_files=("${out_path}/${cell_type}/minimap2_align/"*aligned_genome.bam)
 
-
-for bam in "${out_path}"/"${cell_type}"/minimap2_align/*aligned_genome.bam; do
-    if [ ! -e $(dirname "$bam")/$(basename "$bam" .bam)_filtered.bam ]; then
-      samtools sort -@ 30 -o $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam "$bam"
-      samtools index -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam
-      samtools idxstats -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
-      $(dirname "$bam")/$(basename "$bam" .bam)_idxstats.out
-      samtools flagstat -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
-      $(dirname "$bam")/$(basename "$bam" .bam)_flagstat.out
-      samtools stats -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
-      $(dirname "$bam")/$(basename "$bam" .bam)_stats.out
-      samtools view -@ 30 -q 20 -F 0x904 -b $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam | \
-      samtools sort -@ 30 \
-      -o $(dirname "$bam")/$(basename "$bam" .bam)_filtered.bam
-    fi
-done
+if (( ${#bam_files[@]} > 0 )); then
+  for bam in "${out_path}"/"${cell_type}"/minimap2_align/*aligned_genome.bam; do
+      if [[ ! -e "$(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam" ]]; then
+        samtools sort -@ 30 -o $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam "$bam"
+        samtools index -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam
+        samtools idxstats -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
+        $(dirname "$bam")/$(basename "$bam" .bam)_idxstats.out
+        samtools flagstat -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
+        $(dirname "$bam")/$(basename "$bam" .bam)_flagstat.out
+        samtools stats -@ 30 $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam > \
+        $(dirname "$bam")/$(basename "$bam" .bam)_stats.out
+        samtools view -@ 30 -q 20 -F 0x904 -b $(dirname "$bam")/$(basename "$bam" .bam)_sorted.bam | \
+        samtools sort -@ 30 \
+        -o $(dirname "$bam")/$(basename "$bam" .bam)_filtered.bam
+      fi
+  done
+fi
 
